ff9070d5c7cc69911ddec9929f437db5cbafd974
David Chanin <chanindav@gmail.com>
2011-12-20 22:08:24 -0800

    adding basic rails framework with torquebox template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsChatroom::Application.config.secret_token = '655ffcfcde55764980f2908933c815edbb4cb81436f671b6282a283d29dbadfcc346e7b2eee2ae4d7ce4e54f819bdc6021e12f3a4ccc1528ce60636932d8922a'
