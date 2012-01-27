9258355f6d0e90dfb11569204d3ad3a29c6936eb
David Chanin <chanindav@gmail.com>
2012-01-27 02:25:43 -0800

    trying to get a pre-configured atmosphere servlet class working...

---
+++ lib/atmosphere/RailsAtmosphereServlet.java
@@ -0,0 +1,29 @@
+package atmosphere;
+
+import javax.servlet.ServletConfig;
+import org.atmosphere.cpr.AtmosphereHandler;
+import org.atmosphere.cpr.AtmosphereServlet;
+import org.slf4j.LoggerFactory;
+
+public class RailsAtmosphereServlet extends AtmosphereServlet{
+	
+	private static final Logger logger = LoggerFactory.getLogger(RailsAtmosphereServlet.class);
+	
+	protected void configureWebDotXmlAtmosphereHandler(ServletConfig sc) {
+        String s = sc.getInitParameter("atmosphere.ChatHandler");
+        if (s != null) {
+            ClassLoader cl = Thread.currentThread().getContextClassLoader();
+            try {
+
+                String mapping = sc.getInitParameter("/ChatHandler");
+                if (mapping == null) {
+                    mapping = "/*";
+                }
+                addAtmosphereHandler(mapping, (AtmosphereHandler<?, ?>) cl.loadClass(s).newInstance());
+            } catch (Exception ex) {
+                logger.warn("Unable to load WebSocketHandle instance", ex);
+            }
+        }
+    }
+	
+}
